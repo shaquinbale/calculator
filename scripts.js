6c758cbf8b56ba4510c05e9a15292d20eb3131a6
shaquinbale <scopelenskingdra@gmail.com>
2024-10-20 14:54:22 -0400

    Wrap up project
    fixing various bugs and bringing the project to
    a close

--- scripts.js
+++ scripts.js
@@ -33,29 +33,26 @@ function handleButtonPress(target) {
 
 function handleOperator(id) {
     if (firstArgument === undefined) return;
+    if (operator != undefined) return;
+    if (firstArgumentLocked === true && secondArgument != undefined) handleEquals();
     firstArgumentLocked ? handleEquals : firstArgumentLocked = true
 
 
     switch (id) {
         case 'add':
             operator = '+';
-            display += '+'
             break
         case 'subtract':
             operator = '-';
-            display += '-'
             break
         case 'multiply':
             operator = '*';
-            display += '*'
             break
         case 'divide':
             operator = '/';
-            display += '/'
             break
         case 'modulo':
             operator = '%';
-            display += '%'
             break
     }
 }
@@ -65,15 +62,15 @@ function handleOperand(id) {
 
     if (operator === undefined) {
         firstArgument != undefined ? firstArgument = (firstArgument * 10) + number : firstArgument = firstArgument = number;
-        display += number
+        display = number
     } else {
         secondArgument != undefined ? secondArgument = (secondArgument * 10) + number : secondArgument = secondArgument = number
-        display += number
+        display = number
     }
 }
 
 function handleDecimal() {
-    
+    // add decimal functionality
 }
 
 function handleEquals() {
@@ -106,14 +103,17 @@ function handleEquals() {
 
 function handleSign() {
     firstArgumentLocked ? secondArgument *= -1 : firstArgument *= -1;
-    // add display change
+    //add display change
 }
 
 function handleClear() {
-    
+    firstArgument = undefined;
+    secondArgument = undefined;
+    operator = undefined;
+    display = '';
+    firstArgumentLocked = false;
+    decimalFlag = false;
 }
-const firstCounter = document.querySelector(".firstarg")
-const secondCounter = document.querySelector(".secondarg")
 
 const screen = document.querySelector('#screen')
 const calculator = document.querySelector("#calculator")
@@ -122,6 +122,4 @@ calculator.addEventListener('click', event => {
     handleButtonPress(target);
 
     screen.textContent = display;
-    firstCounter.textContent = `first arg is ${firstArgument}`;
-    secondCounter.textContent = ` second arg is ${secondArgument}`;
 })
